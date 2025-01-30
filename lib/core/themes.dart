edd5f8094b9cfae3e8e62d34e2af95d8b3459967
berrinyiilmaz <bryylmz27@gmail.com>
2025-01-31 00:37:59 +0300

    update

--- lib/core/themes.dart
+++ lib/core/themes.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDark = false;
+  bool get isDark => _isDark;
+
+  void toggleTheme() {
+    _isDark = !_isDark;
+    notifyListeners();
+  }
+}
+
+
+
 
 class AppThemes {
-  // Açık Tema
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    primaryColor: Color.fromARGB(255, 204, 141, 187),
+    primaryColor: Colors.orange,
     scaffoldBackgroundColor: Colors.white,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Color.fromARGB(255, 204, 141, 187),
-      foregroundColor: Colors.white,
-    ),
-    iconTheme: const IconThemeData(color: Colors.black),
-    textTheme: const TextTheme(
-      bodyLarge: TextStyle(color: Colors.black),
-      bodyMedium: TextStyle(color: Colors.black),
-    ),
   );
 
-  // Koyu Tema
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    primaryColor: const Color.fromRGBO(255, 204, 141, 187),
-    scaffoldBackgroundColor: Color.fromARGB(255, 25, 25, 25), // Siyah arka plan
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Color.fromARGB(255, 40, 40, 40),
-      foregroundColor: Colors.white,
-    ),
-    iconTheme: const IconThemeData(color: Colors.white),
-    textTheme: const TextTheme(
-      bodyLarge: TextStyle(color: Colors.white),
-      bodyMedium: TextStyle(color: Colors.white),
+    primaryColor: Colors.blueGrey,
+    scaffoldBackgroundColor: Colors.black,
+  );
+}
+
+
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(builder: (context, themeProvider, child) {
+      return MaterialApp.router(
+        title: 'Wander Wise',
+        themeMode: themeProvider.isDark ? ThemeMode.dark : ThemeMode.light,
+      
+      
+        
+        
+        
+       
+      );
+    });
+  }
 }
